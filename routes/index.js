60324daffc2e4aecd2030420c4b3020f8ba92874
waterman001 <56588553+waterman001@users.noreply.github.com>
2020-05-16 06:13:39 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,35 +2,37 @@ const express = require("express");
 
 const router = express.Router();
 
-const users = [];
+const items = [];
 let id = 1;
 
-router.get('/users', (req, res) =>{
-    res.json(users)
+router.get('/items', (require, response) =>{
+    response.json(items)
 });
 
-router.get('/users/:id', (req, res) =>{
-const user =users.find(val => val.id === Number(req.params.id))
-    res.json(user)
+router.get('/items/:id', (require, response) =>{
+const item = items.find(val => val.id === Number(require.params.id))
+    response.json(item)
 });
 
-router.post("/users", (req, res) =>{
-    users.push({
-        name: req.body.name,
+router.post("/items", (require, response) =>{
+    items.push({
+        name: require.body.name,
+        price: require.body.price,
         id: ++id
     });
-    res.json({ message: " user created" });
+    response.json({ message: " item created" });
 });
 
-router.patch("/users/:id", (require, response) =>{
-    const user = users.find(val => val.id === Number(req.params.id));
-    user.name = require.body.name;
+router.patch("/items/:id", (require, response) =>{
+    const item =items.find(val => val.id === Number(require.params.id));
+    item.name = require.body.name;
+    item.price = require.body.price
         response.json({ message: "updated" });
     });
 
-    router.delete("/users/:id", (req, res) =>{
-        const userIndex =users.findIndex(val => val.id === Number(req.params.id))
-        users.splice(userIndex, 1)
+    router.delete("/items/:id", (req, res) =>{
+        const itemIndex =items.findIndex(val => val.id === Number(req.params.id))
+        items.splice(itemIndex, 1)
         res.json({ message: "deleted" })
 
         });
